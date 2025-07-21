da9948c78868a103b97b8170926b8008b7d9e4bd
Muhammad-Magomedov <magomedov951@mail.ru>
2025-07-21 17:20:54 +0300

    дополнительное задание

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type Post struct {
@@ -12,6 +13,10 @@ type Post struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Body   string `json:"body"`
+}
+
+type HttpResponse struct {
+	Json Post `json:"json"`
 }
 
 func main() {
@@ -38,16 +43,59 @@ func main() {
 
 	fmt.Printf("%+v", allPosts)
 
-	bytes, err := io.ReadAll(resp.Body)
+	responseBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Printf("Ошибка: %v", err)
 	}
 
 	post := Post{}
-	err = json.Unmarshal(bytes, &post)
+	err = json.Unmarshal(responseBytes, &post)
 	if err != nil {
 		fmt.Printf("Не смогли анмаршалить структуру %v", err)
 	}
 
 	fmt.Printf("%+v", post)
+	HomeWorkPost()
+}
+
+func HomeWorkPost() {
+	const url = "https://httpbin.org/post"
+
+	var mockPost = Post{
+		UserID: 1,
+		ID:     1,
+		Title:  "test",
+		Body:   "test123",
+	}
+
+	requestBody := strings.NewReader(`
+	{
+	"UserID": 1,
+	"ID": 1,
+	"Title": "test",
+	"Body": "test123"
+	}
+	`)
+
+	resp, err := http.Post(url, "application/json", requestBody)
+	if err != nil {
+		fmt.Printf("Error cant request %v", err)
+	}
+
+	responseBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Ошибка: %v", err)
+	}
+
+	httpResponse := HttpResponse{}
+	err = json.Unmarshal(responseBytes, &httpResponse)
+	if err != nil {
+		fmt.Printf("Не смогли анмаршалить структуру %v", err)
+	}
+
+	if (mockPost.ID == httpResponse.Json.ID) && (mockPost.Body == httpResponse.Json.Body) && (mockPost.Title == httpResponse.Json.Title) && (mockPost.UserID == httpResponse.Json.UserID) {
+		fmt.Print("Success")
+	}
+
+	fmt.Printf("%+v", httpResponse)
 }
