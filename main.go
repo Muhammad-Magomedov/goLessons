458062577b3e30f872628c4b9bbf3f030195d5cb
Muhammad-Magomedov <magomedov951@mail.ru>
2025-07-22 13:01:04 +0300

    заменил на маршал new reader

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 )
 
 type Post struct {
@@ -20,45 +20,6 @@ type HttpResponse struct {
 }
 
 func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
-	if err != nil {
-		fmt.Printf("ERROR: %v", err)
-	}
-
-	responses, err := http.Get("https://jsonplaceholder.typicode.com/posts")
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-	}
-
-	postsBytes, err := io.ReadAll(responses.Body)
-	if err != nil {
-		fmt.Printf("Ошибка %v", err)
-	}
-
-	allPosts := []Post{}
-	err = json.Unmarshal(postsBytes, &allPosts)
-	if err != nil {
-		fmt.Printf("Не смогли анмаршалить структуру постов %v", err)
-	}
-
-	fmt.Printf("%+v", allPosts)
-
-	responseBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Ошибка: %v", err)
-	}
-
-	post := Post{}
-	err = json.Unmarshal(responseBytes, &post)
-	if err != nil {
-		fmt.Printf("Не смогли анмаршалить структуру %v", err)
-	}
-
-	fmt.Printf("%+v", post)
-	HomeWorkPost()
-}
-
-func HomeWorkPost() {
 	const url = "https://httpbin.org/post"
 
 	var mockPost = Post{
@@ -68,16 +29,12 @@ func HomeWorkPost() {
 		Body:   "test123",
 	}
 
-	requestBody := strings.NewReader(`
-	{
-	"UserID": 1,
-	"ID": 1,
-	"Title": "test",
-	"Body": "test123"
+	jsonData, err := json.Marshal(mockPost)
+	if err != nil {
+		fmt.Printf("Error, cant marshal data to json %v", err)
 	}
-	`)
 
-	resp, err := http.Post(url, "application/json", requestBody)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		fmt.Printf("Error cant request %v", err)
 	}
